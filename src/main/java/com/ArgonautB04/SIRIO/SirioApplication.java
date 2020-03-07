1326f4d05f0f4e16110c02d5684a883e88f9b8a7
Nathanael <nathanael71@ui.ac.id>
2020-03-07 22:14:22 +0700

    init: spring project

---
+++ src/main/java/com/ArgonautB04/SIRIO/SirioApplication.java
@@ -0,0 +1,13 @@
+package com.ArgonautB04.SIRIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirioApplication.class, args);
+	}
+
+}
