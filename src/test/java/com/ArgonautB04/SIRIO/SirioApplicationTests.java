7264c150051570e1e9ce3eaa06a4eea1a2b3bde3
Nathanael <nathanael71@ui.ac.id>
2020-03-07 22:14:22 +0700

    init: spring project

---
+++ src/test/java/com/ArgonautB04/SIRIO/SirioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArgonautB04.SIRIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
