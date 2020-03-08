ce91d8bdb0f2f5b7e5a44c1ad4c7d4b2a7a30add
Nathanael <nathanael71@ui.ac.id>
2020-03-08 19:40:11 +0700

    init: create Rekomendasi

---
+++ src/main/java/com/ArgonautB04/SIRIO/repository/RekomendasiRepo.java
@@ -0,0 +1,9 @@
+package com.ArgonautB04.SIRIO.repository;
+
+import com.ArgonautB04.SIRIO.model.Rekomendasi;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RekomendasiRepo extends JpaRepository<Rekomendasi, Integer> {
+}
