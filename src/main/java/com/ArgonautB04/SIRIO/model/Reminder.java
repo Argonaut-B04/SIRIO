c12abb7babdf7dcbfacdf327d576162769d9c557
Nathanael <nathanael71@ui.ac.id>
2020-03-08 15:04:06 +0700

    init: create Reminder

---
+++ src/main/java/com/ArgonautB04/SIRIO/model/Reminder.java
@@ -0,0 +1,55 @@
+package com.ArgonautB04.SIRIO.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class Reminder implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_reminder;
+
+    @NotNull
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
+    @Column(nullable = false)
+    private LocalDate tanggal_pengiriman;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "pembuat", referencedColumnName = "id_employee", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Employee pembuat;
+
+    public int getId_reminder() {
+        return id_reminder;
+    }
+
+    public void setId_reminder(int id_reminder) {
+        this.id_reminder = id_reminder;
+    }
+
+    public LocalDate getTanggal_pengiriman() {
+        return tanggal_pengiriman;
+    }
+
+    public void setTanggal_pengiriman(LocalDate tanggal_pengiriman) {
+        this.tanggal_pengiriman = tanggal_pengiriman;
+    }
+
+    public Employee getPembuat() {
+        return pembuat;
+    }
+
+    public void setPembuat(Employee pembuat) {
+        this.pembuat = pembuat;
+    }
+}
