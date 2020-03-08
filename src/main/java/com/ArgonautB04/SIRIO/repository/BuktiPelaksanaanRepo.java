f95a2017d2a705f86181af0560be42f9c45ceb77
Nathanael <nathanael71@ui.ac.id>
2020-03-08 19:49:54 +0700

    init: create Bukti Pelaksanaan

---
+++ src/main/java/com/ArgonautB04/SIRIO/repository/BuktiPelaksanaanRepo.java
@@ -0,0 +1,9 @@
+package com.ArgonautB04.SIRIO.repository;
+
+import com.ArgonautB04.SIRIO.model.BuktiPelaksanaan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BuktiPelaksanaanRepo extends JpaRepository<BuktiPelaksanaan, Integer> {
+}
