347e86d120dc28644ebcc828bba8a0b8e56ae4c8
Nathanael <nathanael71@ui.ac.id>
2020-03-09 10:45:59 +0700

    feat: Status Rekomendasi Service

---
+++ BackEnd/src/main/java/com/ArgonautB04/SIRIO/services/StatusRekomendasiRestServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ArgonautB04.SIRIO.services;
+
+import com.ArgonautB04.SIRIO.model.StatusRekomendasi;
+import com.ArgonautB04.SIRIO.repository.StatusRekomendasiRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class StatusRekomendasiRestServiceImpl implements StatusRekomendasiRestService {
+
+    @Autowired
+    private StatusRekomendasiRepo statusRekomendasiRepo;
+
+    @Override
+    public StatusRekomendasi createStatusRekomendasi(StatusRekomendasi statusRekomendasi) {
+        return statusRekomendasiRepo.save(statusRekomendasi);
+    }
+
+    @Override
+    public StatusRekomendasi getById(int id_status) {
+        Optional<StatusRekomendasi> statusRekomendasi = statusRekomendasiRepo.findById(id_status);
+        if (statusRekomendasi.isPresent()) {
+            return statusRekomendasi.get();
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+}
