a76fe468f1504445362bcefdca4251a344eaeb5b
Nathanael <nathanael71@ui.ac.id>
2020-03-09 07:08:43 +0700

    feat: Role Service

---
+++ BackEnd/src/main/java/com/ArgonautB04/SIRIO/services/RoleRestServiceImpl.java
@@ -0,0 +1,45 @@
+package com.ArgonautB04.SIRIO.services;
+
+import com.ArgonautB04.SIRIO.model.Role;
+import com.ArgonautB04.SIRIO.repository.RoleRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class RoleRestServiceImpl implements RoleRestService {
+
+    @Autowired
+    private RoleRepo roleRepo;
+
+    @Override
+    public Role createRole(Role role) {
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public Role getById(int id_role) {
+        Optional<Role> role = roleRepo.findById(id_role);
+        if (role.isPresent()) {
+            return role.get();
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public Role updateRole(int id_role, Role role) {
+        Role target = getById(id_role);
+        target.setNama_role(role.getNama_role());
+        return roleRepo.save(target);
+    }
+
+    @Override
+    public void deleteRole(int id_role) {
+        roleRepo.deleteById(id_role);
+    }
+}
