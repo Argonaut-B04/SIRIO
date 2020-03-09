756397aa709b0644bec93d2b861194da3957b940
Nathanael <nathanael71@ui.ac.id>
2020-03-09 10:43:24 +0700

    feat: Status Hasil Pemeriksaan Service

---
+++ BackEnd/src/main/java/com/ArgonautB04/SIRIO/services/StatusHasilPemeriksaanRestService.java
@@ -0,0 +1,8 @@
+package com.ArgonautB04.SIRIO.services;
+
+import com.ArgonautB04.SIRIO.model.StatusHasilPemeriksaan;
+
+public interface StatusHasilPemeriksaanRestService {
+    StatusHasilPemeriksaan createStatusHasilPemeriksaan (StatusHasilPemeriksaan statusHasilPemeriksaan);
+    StatusHasilPemeriksaan getById(int id_status);
+}
