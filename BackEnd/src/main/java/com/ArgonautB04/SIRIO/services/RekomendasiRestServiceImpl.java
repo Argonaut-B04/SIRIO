1f05a5be9a6292afdab9bcc3a7875b5f758c739e
Nathanael <nathanael71@ui.ac.id>
2020-03-11 22:14:34 +0700

    feat: create Rekomendasi Functions and revise old services

--- BackEnd/src/main/java/com/ArgonautB04/SIRIO/services/RekomendasiRestServiceImpl.java
+++ BackEnd/src/main/java/com/ArgonautB04/SIRIO/services/RekomendasiRestServiceImpl.java
@@ -1,11 +1,14 @@
 package com.ArgonautB04.SIRIO.services;
 
+import com.ArgonautB04.SIRIO.model.Employee;
 import com.ArgonautB04.SIRIO.model.Rekomendasi;
+import com.ArgonautB04.SIRIO.model.Reminder;
 import com.ArgonautB04.SIRIO.repository.RekomendasiDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -49,5 +52,17 @@ public class RekomendasiRestServiceImpl implements RekomendasiRestService {
     @Override
     public void hapusRekomendasi(int idRekomendasi) {
         rekomendasiDB.deleteById(idRekomendasi);
+    }
+
+    @Override
+    public Rekomendasi ubahTenggatWaktu(int idRekomendasi, LocalDate tenggatWaktuLocalDate) {
+        Rekomendasi target = getById(idRekomendasi);
+        target.setTenggatWaktu(tenggatWaktuLocalDate);
+        return rekomendasiDB.save(target);
+    }
+
+    @Override
+    public List<Rekomendasi> getByPembuat(Employee pembuat) {
+        return rekomendasiDB.findAllByPembuat(pembuat);
     }
 }
