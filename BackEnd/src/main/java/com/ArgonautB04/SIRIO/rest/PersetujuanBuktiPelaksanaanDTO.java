3a4f9f5d71125f770d692ed815923b8c00aa160a
Anastasia Greta <agretaelena@gmail.com>
2020-03-19 15:18:33 +0700

    feat: add PersetujuanBuktiPelaksanaan DTO

---
+++ BackEnd/src/main/java/com/ArgonautB04/SIRIO/rest/PersetujuanBuktiPelaksanaanDTO.java
@@ -0,0 +1,40 @@
+package com.ArgonautB04.SIRIO.rest;
+
+public class PersetujuanBuktiPelaksanaanDTO {
+    private Integer idPemeriksa;
+    private Integer idBuktiPelaksanaan;
+    private Integer status;
+    private String feedback;
+
+    public Integer getIdPemeriksa() {
+        return idPemeriksa;
+    }
+
+    public void setIdPemeriksa(Integer idPemeriksa) {
+        this.idPemeriksa = idPemeriksa;
+    }
+
+    public Integer getIdBuktiPelaksanaan() {
+        return idBuktiPelaksanaan;
+    }
+
+    public void setIdBuktiPelaksanaan(Integer idBuktiPelaksanaan) {
+        this.idBuktiPelaksanaan = idBuktiPelaksanaan;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}
