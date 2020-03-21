5832ace787a1882748dd4d72b533898e8fcc144d
Nathanael <nathanael71@ui.ac.id>
2020-03-21 10:12:56 +0700

    feat: generalizing Tables

--- FrontEnd/src/Components/Tables/Rekomendasi/TabelRekomendasi.jsx
+++ FrontEnd/src/Components/Tables/Rekomendasi/TabelRekomendasi.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import BootstrapTable from "react-bootstrap-table-next";
-import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css"
-import paginationFactory from 'react-bootstrap-table2-paginator';
-import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
-import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
-import overlayFactory from 'react-bootstrap-table2-overlay';
 import SirioButton from '../../Button/SirioButton';
 import SirioDatePickerButton from '../../Button/SirioDatePickerButton';
 import classes from './TabelRekomendasi.module.css';
+import SirioTable from '../SirioTable';
 
 export default class TabelRekomendasi extends React.Component {
-    indication() {
-        return "No Data in Table"
-    }
 
     statusFormatter(cell) {
         switch (cell) {
@@ -43,12 +35,6 @@ export default class TabelRekomendasi extends React.Component {
             default:
                 return (cell);
         }
-    }
-
-    rowNumber(cell, row, rowIndex, formatExtraData) {
-        return (
-            <>{rowIndex + 1}</>
-        )
     }
 
     getButtonsFirst(cell, row) {
@@ -109,8 +95,8 @@ export default class TabelRekomendasi extends React.Component {
         formatter: this.rowNumber
 
     }, {
-        dataField: 'namaRekomendasi',
-        text: 'NAMA',
+        dataField: 'keteranganRekomendasi',
+        text: 'KETERANGAN',
         sort: true,
         classes: classes.rowItem,
         headerClasses: classes.colheader,
@@ -157,107 +143,39 @@ export default class TabelRekomendasi extends React.Component {
         formatter: this.getButtonsThird
     }];
 
-    products = [
-        { "tenggatWaktu": "", "id": 10, "namaRekomendasi": "Nathan", "statusRekomendasi": "Menunggu Pengaturan Tenggat Waktu" },
-        { "tenggatWaktu": "", "id": 1, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Ditolak" },
-        { "tenggatWaktu": "", "id": 2, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Sedang Dijalankan" },
-        { "tenggatWaktu": "", "id": 3, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "10-10-2000", "id": 4, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Menunggu Pelaksanaan" },
-        { "tenggatWaktu": "10-10-2000", "id": 5, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Sedang Dijalankan" },
-        { "tenggatWaktu": "", "id": 6, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
-        { "tenggatWaktu": "", "id": 7, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
-        { "tenggatWaktu": "", "id": 8, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
-        { "tenggatWaktu": "", "id": 9, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
-        { "tenggatWaktu": "", "id": 11, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
-        { "tenggatWaktu": "", "id": 12, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
-        { "tenggatWaktu": "", "id": 13, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
-        { "tenggatWaktu": "", "id": 14, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
-        { "tenggatWaktu": "", "id": 25, "namaRekomendasi": "Nathan2", "statusRekomendasi": "Draft" },
-        { "tenggatWaktu": "", "id": 16, "namaRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
+    data = [
+        { "tenggatWaktu": "", "id": 10, "keteranganRekomendasi": "Nathan", "statusRekomendasi": "Menunggu Pengaturan Tenggat Waktu" },
+        { "tenggatWaktu": "", "id": 1, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Ditolak" },
+        { "tenggatWaktu": "", "id": 2, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Sedang Dijalankan" },
+        { "tenggatWaktu": "", "id": 3, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Menunggu Persetujuan" },
+        { "tenggatWaktu": "10/10/2000", "id": 4, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Menunggu Pelaksanaan" },
+        { "tenggatWaktu": "10/10/2000", "id": 5, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Sedang Dijalankan" },
+        { "tenggatWaktu": "", "id": 6, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
+        { "tenggatWaktu": "", "id": 7, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
+        { "tenggatWaktu": "", "id": 8, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
+        { "tenggatWaktu": "", "id": 9, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
+        { "tenggatWaktu": "", "id": 11, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
+        { "tenggatWaktu": "", "id": 12, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
+        { "tenggatWaktu": "", "id": 13, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
+        { "tenggatWaktu": "", "id": 14, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
+        { "tenggatWaktu": "", "id": 25, "keteranganRekomendasi": "Nathan2", "statusRekomendasi": "Draft" },
+        { "tenggatWaktu": "", "id": 16, "keteranganRekomendasi": "Nathan1", "statusRekomendasi": "Draft" },
     ]
-
 
     defaultSorted = [{
         dataField: 'id',
         order: 'asc'
     }];
 
-    customTotal = (from, to, size) => (
-        <span className="react-bootstrap-table-pagination-total text-secondary">
-            Show {from} to {to} of {size} entries
-        </span>
-    );
-
-    pageButtonRenderer = ({
-        page,
-        active,
-        disabled,
-        title,
-        onPageChange
-    }) => {
-        const handleClick = (e) => {
-            e.preventDefault();
-            onPageChange(page);
-        };
-        return (
-            <li className="page-item" key={page} >
-                <a href="https://www.google.com/" className="sirio-pagination-button" onClick={handleClick}>{page}</a>
-            </li>
-        );
-    };
-
-    pagination = paginationFactory({
-        sizePerPage: 10,
-        prePageText: "Previous",
-        nextPageText: "Next",
-        showTotal: true,
-        paginationTotalRenderer: this.customTotal,
-        pageButtonRenderer: this.pageButtonRenderer
-    })
-
-    overlay = overlayFactory({ spinner: true, styles: { overlay: (base) => ({ ...base, background: 'rgba(255, 0, 0, 0.5)' }) } })
-
     render() {
-        const { indication, products, columns, defaultSorted, pagination, overlay } = this;
-        const { SearchBar } = Search;
         return (
-            <div>
-                <h1 className={classes.title}>Daftar Rekomendasi</h1>
-                <div className={classes.toolkitWrapper}>
-                    <ToolkitProvider
-                        bootstrap4
-                        keyField='id'
-                        data={products}
-                        columns={columns}
-                        search
-                    >
-                        {
-                            props => (
-                                <div>
-                                    <div className="text-right">
-                                        <label className="m-2">Search: </label>
-                                        <SearchBar
-                                            {...props.searchProps}
-                                            placeholder=""
-                                        />
-                                    </div>
-                                    <BootstrapTable
-                                        {...props.baseProps}
-                                        ref={n => this.node = n}
-                                        striped
-                                        hover
-                                        condensed
-                                        noDataIndication={indication}
-                                        defaultSorted={defaultSorted}
-                                        pagination={pagination}
-                                        overlay={overlay}
-                                    />
-                                </div>
-                            )
-                        }
-                    </ToolkitProvider>
-                </div>
-            </div>
+            <SirioTable
+                title="Daftar Rekomendasi"
+                data={this.data}
+                id='id'
+                columnsDefinition={this.columns}
+                includeSearchBar
+            />
         );
     }
 } 
---
+++ FrontEnd/src/Components/Tables/SirioTable.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+import overlayFactory from 'react-bootstrap-table2-overlay';
+import classes from './SirioTable.module.css';
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css"
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+
+class SirioTable extends Component {
+
+    indication() {
+        return "No Data in Table"
+    }
+
+    customTotal = (from, to, size) => (
+        <span className="react-bootstrap-table-pagination-total text-secondary ml-5">
+            Show {from} to {to} of {size} entries
+        </span>
+    );
+
+    pageButtonRenderer = ({
+        page,
+        active,
+        disabled,
+        title,
+        onPageChange
+    }) => {
+        const handleClick = (e) => {
+            e.preventDefault();
+            onPageChange(page);
+        };
+        return (
+            <li className="page-item" key={page} >
+                <a href="https://www.google.com/" className="sirio-pagination-button" onClick={handleClick}>{page}</a>
+            </li>
+        );
+    };
+
+    pagination = paginationFactory({
+        sizePerPage: 10,
+        prePageText: "Previous",
+        nextPageText: "Next",
+        showTotal: true,
+        paginationTotalRenderer: this.customTotal,
+        // pageButtonRenderer: this.pageButtonRenderer
+    })
+
+    overlay = overlayFactory({
+        spinner: true,
+        styles: {
+            overlay: (base) => (
+                {
+                    ...base,
+                    background: 'rgba(255, 0, 0, 0.5)'
+                }
+            )
+        }
+    })
+
+    render() {
+        const { SearchBar } = Search;
+        return (
+            <div>
+                <h1 className={classes.title}>{this.props.title}</h1>
+                <div className={classes.toolkitWrapper}>
+                    <ToolkitProvider
+                        bootstrap4
+                        keyField={this.props.id}
+                        data={this.props.data}
+                        columns={this.props.columnsDefinition}
+                        search
+                    >
+                        {
+                            props => (
+                                <div>
+                                    {this.props.includeSearchBar ?
+                                        <div className="text-right">
+                                            <label className="m-2">Search: </label>
+                                            <SearchBar
+                                                {...props.searchProps}
+                                                placeholder=""
+                                            />
+                                        </div>
+                                        :
+                                        null
+                                    }
+                                    <BootstrapTable
+                                        {...props.baseProps}
+                                        ref={n => this.node = n}
+                                        striped
+                                        hover
+                                        condensed
+                                        noDataIndication={this.indication}
+                                        defaultSorted={this.defaultSorted}
+                                        pagination={this.pagination}
+                                        overlay={this.overlay}
+                                    />
+                                </div>
+                            )
+                        }
+                    </ToolkitProvider>
+                </div>
+            </div>
+        );
+    }
+
+};
+export default SirioTable;
