6928997dd7f8394402ec30d59bae66bc6da68451
Anastasia Greta <agretaelena@gmail.com>
2020-03-22 20:19:30 +0700

    frontend tabel rekomendasi untuk branch manager init

--- FrontEnd/src/Components/Tables/Rekomendasi/TabelRekomendasiBM.jsx
+++ FrontEnd/src/Components/Tables/Rekomendasi/TabelRekomendasiBM.jsx
@@ -59,8 +59,8 @@ export default class TabelRekomendasi extends React.Component {
         }
 
     }, {
-        dataField: 'tenggatWaktuRekomendasi',
-        text: 'TENGGAT WAKTU',
+        dataField: 'Rekomendasi',
+        text: 'KETERANGAN',
         sort: true,
         classes: classes.rowItem,
         headerClasses: classes.colheader,
@@ -100,22 +100,22 @@ export default class TabelRekomendasi extends React.Component {
     }];
 
     data = [
-        { "tenggatWaktu": "", "id": 10, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 1, "keteranganRekomendasi": "Keterangan", "statusBukti": "Ditolak" },
-        { "tenggatWaktu": "", "id": 2, "keteranganRekomendasi": "Keterangan", "statusBukti": "Disetujui" },
-        { "tenggatWaktu": "", "id": 3, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "10/10/2000", "id": 4, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "10/10/2000", "id": 5, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 6, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 7, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 8, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 9, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 11, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 12, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 13, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 14, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 25, "keteranganRekomendasi": "Nathan2", "statusBukti": "Menunggu Persetujuan" },
-        { "tenggatWaktu": "", "id": 16, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 10, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 1, "keteranganRekomendasi": "Keterangan", "statusBukti": "Ditolak" },
+        { "id": 2, "keteranganRekomendasi": "Keterangan", "statusBukti": "Disetujui" },
+        { "id": 3, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "": "10/10/2000", "id": 4, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "": "10/10/2000", "id": 5, "keteranganRekomendasi": "Keterangan", "statusBukti": "Sedang Persetujuan" },
+        { "id": 6, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 7, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 8, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 9, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 11, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 12, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 13, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 14, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 25, "keteranganRekomendasi": "Keterangan2", "statusBukti": "Menunggu Persetujuan" },
+        { "id": 16, "keteranganRekomendasi": "Keterangan", "statusBukti": "Menunggu Persetujuan" },
     ]
 
     defaultSorted = [{
