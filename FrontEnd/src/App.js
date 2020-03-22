2458aa89125db566d21ceb7012bc9db1f4eb462f
Nathanael <nathanael71@ui.ac.id>
2020-03-22 07:39:00 +0700

    feat: create frontend login logout mechanism

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -10,6 +10,7 @@ import MainPage from "./pages";
 import DaftarRekomendasi from "./pages/DaftarRekomendasi";
 import NotFound from "./pages/error";
 import Login from "./pages/login";
+import Logout from "./pages/logout";
 import './App.css';
 
 class App extends React.Component {
@@ -21,6 +22,7 @@ class App extends React.Component {
             <Route exact path="/" component={MainPage} />
             <Route exact path="/rekomendasi" component={DaftarRekomendasi} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/logout" component={Logout} />
             <Route exact path="/404" component={NotFound} />
             <Redirect to="/404" />
           </Switch>
--- FrontEnd/src/pages/index.jsx
+++ FrontEnd/src/pages/index.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import {
     Link
-} from 'react-router-dom'
+} from 'react-router-dom';
+import AuthenticationService from "../Services/AuthenticationService";
 
 export default class MainPage extends React.Component {
     render() {
+        let username = AuthenticationService.getUsername();
+        let role = AuthenticationService.getRole();
         return (
             <div>
                 <h3>Welcome to SIRIO</h3>
+                {username && <h4>Username: {username}</h4>}
+                {role && <h4>Role: {role}</h4>}
                 <small>Main Page</small>
                 <br></br>
                 <Link to="/rekomendasi" className="btn btn-primary">Tabel Rekomendasi</Link>
                 <Link to="/login" className="btn btn-primary">Login</Link>
+                <Link to="/logout" className="btn btn-primary">Logout</Link>
             </div>
         )
     }
