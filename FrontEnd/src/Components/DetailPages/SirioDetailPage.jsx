2d2f8e8a8646cf4cb0e1d44efe65059ba1b586d9
Nathanael <nathanael71@ui.ac.id>
2020-04-01 06:23:46 +0800

    feat: refurnish detail page component

--- FrontEnd/src/Components/DetailPages/SirioDetailPage.jsx
+++ FrontEnd/src/Components/DetailPages/SirioDetailPage.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import BootstrapTable from "react-bootstrap-table-next";
-import ToolkitProvider from 'react-bootstrap-table2-toolkit';
+import { Table } from 'react-bootstrap';
 import classes from './SirioDetailPage.module.css';
-import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css"
-import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 
 class SirioDetailPage extends Component {
 
-    render() {       
-        return ( 
+    render() {
+        return (
             <div>
                 <div className={classes.headerWrapper}>
                     <h2 className={classes.title}>
@@ -16,25 +13,16 @@ class SirioDetailPage extends Component {
                     </h2>
                 </div>
                 <div className={classes.toolkitWrapper}>
-                    <ToolkitProvider
-                        bootstrap4
-                        keyField={this.props.id}
-                        data={this.props.data}
-                        columns={this.props.columnsDefinition}
-                    >
-                        {
-                            props => (
-                                <div>
-                                    <BootstrapTable
-                                        {...props.baseProps}
-                                        bordered={ false }
-                                    />
-                                    
-                                </div>
-                            )
-                        }
-                    </ToolkitProvider>
-                    <br></br>
+                    <Table responsive>
+                        <tbody>
+                            {Object.keys(this.props.data).map((key, i) =>
+                                <tr key={i}>
+                                    <td className={classes.rowItem}>{key}</td>
+                                    <td className={classes.rowItem}>{this.props.data[key]}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </Table>
                     <div className="w-100 text-right">
                         {this.props.subButton}
                     </div>
