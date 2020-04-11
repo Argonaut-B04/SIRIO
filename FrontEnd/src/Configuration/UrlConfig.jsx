28729c5145364f7937805bb4432cc3f73eaa396b
Nathanael <nathanael71@ui.ac.id>
2020-04-11 22:44:47 +0800

    feat: generalizing configuration file

---
+++ FrontEnd/src/Configuration/UrlConfig.jsx
@@ -0,0 +1,89 @@
+export const halamanUtama = {
+    link: "/",
+    title: "Halaman Utama",
+}
+
+export const hasilPemeriksaan = {
+    link: "/hasil",
+    title: "Hasil Pemeriksaan",
+}
+
+export const tugasPemeriksaan = {
+    link: "/tugas",
+    title: "Tugas Pemeriksaan",
+}
+
+export const rekomendasi = {
+    link: "/rekomendasi",
+    title: "Rekomendasi",
+}
+
+export const buktiRekomendasi = {
+    link: "/bukti",
+    title: "Bukti Rekomendasi",
+}
+
+export const dashboard = {
+    link: "/dash",
+    title: "Dashboard",
+}
+
+export const rencanaPemeriksaan = {
+    link: "/rencana",
+    title: "Rencana Pemeriksaan",
+}
+
+export const konfigurasi = {
+    link: "/konfigurasi",
+    title: "Konfigurasi",
+    dropdown: [
+        {
+            title: "Risk Rating",
+            link: "#"
+        }, {
+            title: "Registrasi Risiko",
+            link: "#"
+        }, {
+            title: "Risk Level",
+            link: "#"
+        }, {
+            title: "SOP",
+            link: "#"
+        }
+    ]
+}
+
+export const pengguna = {
+    link: "/pengguna",
+    title: "Pengguna",
+}
+
+export const kantorCabang = {
+    link: "/kantor-cabang",
+    title: "Kantor Cabang",
+}
+
+export const profilKC = {
+    link: "/profil-kantor",
+    title: "Profil Kantor Cabang",
+}
+
+export const dashboardEmployee = {
+    link: "/dashboard-employee",
+    title: "Dashboard Employee",
+}
+
+export const dashboardKantorCabang = {
+    link: "/dashboard-kantor",
+    title: "Dashboard Kantor Cabang",
+}
+
+export const login = {
+    link: "/login",
+    title: "Login"
+}
+
+export const logout = {
+    link: "/logout",
+    title: "Logout"
+}
