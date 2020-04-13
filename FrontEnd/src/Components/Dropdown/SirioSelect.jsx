321f1000829249cedd4f172694ea1bee2fc63473
Nathanael Lie <nathanael71@ui.ac.id>
2020-04-13 12:33:53 +0800

    fix: add SirioSelect support when options not given

--- FrontEnd/src/Components/Dropdown/SirioSelect.jsx
+++ FrontEnd/src/Components/Dropdown/SirioSelect.jsx
@@ -38,7 +38,7 @@ export default class SirioSelect extends Component {
     render() {
         return (
             <Select
-                value={this.props.options.filter(option => option.value === this.props.value)}
+                value={this.props.options  && this.props.options.filter(option => option.value === this.props.value)}
                 onChange={event => this.props.handleChange(this.props.name, event, this.props.index)}
                 components={{ IndicatorSeparator:() => null }}
                 options={this.props.options}
