ff155cef7b724618191efa7ebe69f568401db4c6
Nathanael Lie <nathanael71@ui.ac.id>
2020-05-03 10:56:19 +0800

    fix : access permission for hierarki

--- BackEnd/src/main/java/com/ArgonautB04/SIRIO/controller/RisikoRestController.java
+++ BackEnd/src/main/java/com/ArgonautB04/SIRIO/controller/RisikoRestController.java
@@ -222,7 +222,7 @@ public class RisikoRestController {
     @GetMapping("/ubah-hierarki/kategori")
     private BaseResponse<List<List<Risiko>>> getByKategori(Principal principal) {
         Employee employee = employeeRestService.validateEmployeeExistByPrincipal(principal);
-        employeeRestService.validateRolePermission(employee, "tambah risiko");
+        employeeRestService.validateRolePermission(employee, "akses risiko");
 
         List<List<Risiko>> listOfOptionList = new ArrayList<>();
         listOfOptionList.add(
@@ -231,7 +231,6 @@ public class RisikoRestController {
         listOfOptionList.add(
                 risikoRestService.getByKategori(2)
         );
-
         return new BaseResponse<>(200, "success", listOfOptionList);
     }
 }
