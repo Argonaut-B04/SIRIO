7e14e81d5af64e9d80f317fb791d30bf61ee6027
Nathanael <nael.nathanael71@gmail.com>
2020-05-20 16:41:07 +0800

    change url api

--- FrontEnd/src/Services/SirioAxiosBase.jsx
+++ FrontEnd/src/Services/SirioAxiosBase.jsx
@@ -2,7 +2,7 @@ export default class SirioAxiosBase {
     static USERNAME_NAME_SESSION_ATTRIBUTE_NAME = "SirioInSession";
     static ROLE_NAME_SESSION_ATTRIBUTE_NAME = "SirioRoleInSession";
     static TOKEN_SESSION_ATTRIBUTE_NAME = "SirioTokenInSession";
-    static BASEURL = "http://localhost:8080/api/v1";
+    static BASEURL = "http://sirio-api.herokuapp.com/api/v1";
 
     static formatDate(cell) {
         if (cell == null || cell.length < 3) {
